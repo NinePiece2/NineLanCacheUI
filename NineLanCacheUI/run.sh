5b3f0cec606271b65013c48edd999783a53b99ec
Romit Sagu <romit.sagu@hotmail.com>
2025-11-23 04:38:55 -0500

    Try to fix nginx issue

--- NineLanCacheUI/run.sh
+++ NineLanCacheUI/run.sh
@@ -4,8 +4,7 @@
 export API_PORT=${API_PORT:-7401}
 
 # Substitute environment variables in nginx config
-envsubst '${API_PORT}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
-mv /tmp/nginx.conf /etc/nginx/nginx.conf
+envsubst '${API_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Next.js and nginx
 npx concurrently "npm start" "nginx -g 'daemon off;'"
