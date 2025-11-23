7b41a71c19457b6b7111c3d2dbf885582768e94b
Romit Sagu <romit.sagu@hotmail.com>
2025-11-23 04:16:02 -0500

    Fix known issues in #1 #2 and others.

--- NineLanCacheUI/run.sh
+++ NineLanCacheUI/run.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+
+# Set default API_PORT if not provided
+export API_PORT=${API_PORT:-7401}
+
+# Substitute environment variables in nginx config
+envsubst '${API_PORT}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
+mv /tmp/nginx.conf /etc/nginx/nginx.conf
+
+# Start Next.js and nginx
 npx concurrently "npm start" "nginx -g 'daemon off;'"
