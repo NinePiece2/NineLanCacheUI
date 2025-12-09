00d97ebbeebb6c708b3c996f6b3633c6062be624
Romit Sagu <romit.sagu@hotmail.com>
2025-12-08 19:22:29 -0500

    Add the ability to change the Nginx Port

--- NineLanCacheUI/run.sh
+++ NineLanCacheUI/run.sh
@@ -3,8 +3,11 @@
 # Set default API_PORT if not provided
 export API_PORT=${API_PORT:-7401}
 
+# Set default NGINX_PORT if not provided
+export NGINX_PORT=${NGINX_PORT:-8080}
+
 # Substitute environment variables in nginx config
-envsubst '${API_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${API_PORT},${NGINX_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Next.js and nginx
 npx concurrently "npm start" "nginx -g 'daemon off;'"
